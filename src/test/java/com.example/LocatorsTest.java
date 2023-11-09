a5ead122e5b7434a5b048a8576a803e622f0b2f6
Andrew Zola <and.zola.77@gmail.com>
2023-11-09 15:04:08 +0100

    Final commit

--- src/test/java/com.example/LocatorsTest.java
+++ src/test/java/com.example/LocatorsTest.java
@@ -10,7 +10,6 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.interactions.Actions;
-
 import java.util.HashMap;
 import java.util.Map;
 
@@ -42,7 +41,7 @@ public class LocatorsTest {
         String baseUrl = "https://selectorshub.com/xpath-practice-page/";
         // Открываем главную страницу демо-сайта
         driver.get(baseUrl);
-        // Открываем бразуер на полный экран
+        // Открываем браузер на полный экран
         driver.manage().window().maximize();
 
         // Ждём пока страница появится - этот способ подходит только для демонстрации
@@ -55,23 +54,25 @@ public class LocatorsTest {
 
     @Test
     public void testComplexLocators() {
+        //Выберите дату начала наших курсов в календаре, чтобы проверка прошла
         WebElement date = driver.findElement(By.xpath("/html/body/div[1]/main/div/div[1]/section[2]/div/div[2]/div/div/div/input[2]"));
         date.sendKeys("06.06.2023");
-        //Выберите дату начала наших курсов в календаре, чтобы проверка прошла
+        //Проверка
         assertEquals("2023-06-06", driver.findElement(By.xpath("/html/body/div[1]/main/div/div[1]/section[2]/div/div[2]/div/div/div/input[2]")).getAttribute("value"));
+
         //После этого переходим к таблице
         WebElement elementH2 = driver.findElement(By.xpath("//h2[.='System Distribution Details']"));
         new Actions(driver).scrollToElement(elementH2).perform();
+
         //Нужно переключить количество строк в таблице на любое другое значение кроме 10
-        WebElement optionElement = driver.findElement(By.xpath("//*[@id=\"tablepress-1_length\"]/label/select/option[2]"));
         WebElement scrollElement = driver.findElement(By.xpath("//*[@id=\"tablepress-1_length\"]/label/select"));
         scrollElement.click();
         scrollElement.findElement(By.xpath("//*[@id=\"tablepress-1_length\"]/label/select/option[2]")).click();
-        //new Actions(driver).scrollToElement(optionElement).i;
 
+        //И нажать чекбокс на строке с данными "windows	Edge Samsun	India" чтобы проверка прошла
         WebElement noteElement = driver.findElement(By.xpath("//*[@id=\"tablepress-1\"]/tbody/tr[22]/td[1]/input"));
         noteElement.click();
-        //И нажать чекбокс на первой серой строке с данными "windows	Edge	Samsun	India" чтобы проверка прошла
+        //Проверка
         assertTrue(driver.findElement(By.xpath("/html/body/div[1]/main/div/div[1]/section[3]/div/div/div/div[3]/div/div/div/table/tbody/tr[22]/td[1]/input")).isSelected());
     }
 
